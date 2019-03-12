9d1c8a48d3f111baf4d49537858750586bc17f7a
Gavin <ooigavin@gmail.com>
2019-03-12 11:43:11 +0800

    Add pdf format

--- src/Screens/Sizing/CalculateSizing.js
+++ src/Screens/Sizing/CalculateSizing.js
@@ -9,6 +9,7 @@ import {saveSizing} from '../../store/actions/sizingActions';
 import CollapsePanel from '../../UI/CollapsePanel/CollapsePanel';
 import IconButton from '../../UI/IconButton/IconButton';
 import CalcInput from '../../UI/CalcInput/CalcInput';
+import HEImage from '../../assests/images/HE.png';
 
 class CalculateSizing extends Component {
 
@@ -61,12 +62,87 @@ class CalculateSizing extends Component {
   };
 
   generateReport = () => {
+    const htmlContent = `
+<h1>${this.props.planName}</h1>
+<img src="file:////storage/7E3B-15E6/Android/data/com.hedesigner/he.png"/>
+<h3>Input Parameters</h3>
+<table cellpadding="10" border="1|0">
+  <tr>
+      <th>Fluid Parameters</th>
+      <th>Shell-Side</th>
+      <th>Tube-Side</th>
+  </tr>
+  <tr>
+      <td>Inlet Temperature</td>
+      <td>35</td>
+      <td>20</td>
+  </tr>
+  <tr>
+      <td>Outlet Temperature</td>
+      <td>25</td>
+      <td>25</td>
+  </tr>
+  <tr>
+      <td>Mass Flow Rate</td>
+      <td>80,000</td>
+      <td>140,000</td>
+  </tr>
+  <tr>
+      <td>Specific Heat Capacity</td>
+      <td>4178.5</td>
+      <td>4179</td>
+  </tr>
+  <tr>
+      <td>Dynamic Viscosity</td>
+      <td>0.000797</td>
+      <td>0.00095</td>
+  </tr>
+  <tr>
+      <td>Thermal Conductivity</td>
+      <td>0.614</td>
+      <td>0.6065</td>
+  </tr>
+  <tr>
+      <td>Prandtl Number</td>
+      <td>5.43</td>
+      <td>6.55</td>
+  </tr>
+  <tr>
+      <td>Density</td>
+      <td>995.7</td>
+      <td>997</td>
+  </tr>
+</table>
+<h3>Sizing Results</h3>
+<table cellpadding="10" border="1|0">
+  <tr>
+      <th>Parameters</th>
+      <th>Results</th>
+  </tr>
+  <tr>
+      <td>Tube-side heat transfer coefficient</td>
+      <td>4871</td>
+  </tr>
+  <tr>
+      <td>Tube-side pressure drop</td>
+      <td>95,770</td>
+  </tr>
+  <tr>
+      <td>Shell-side heat transfer coefficient</td>
+      <td>4,469</td>
+  </tr>
+  <tr>
+      <td>Shell-side pressure drop</td>
+      <td>61,180</td>
+  </tr>
+</table>
+`
     let options = {
-      html: '<h1>PDF TEST</h1>',
-      fileName: 'test',
+      html: htmlContent,
+      fileName: this.props.planName,
       directory: 'Documents'
     };
-
+    console.log(htmlContent)
     RNHTMLtoPDF.convert(options).then(filePath => {
       console.log('PDF generated', filePath);
     
